7a30191e15925d0877895387f30ed4c7986dfb42
Isuru <isurusajith0@gmail.com>
2024-01-13 09:23:54 +0530

    trigger get all when submitt data

--- script.js
+++ script.js
@@ -17,7 +17,11 @@ function submitForm() {
         body: JSON.stringify(formData)
     })
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+        //displayData(data);
+        getAllData();
+    })
     .catch(error => console.error('Error:', error));
 }
 
@@ -27,22 +31,47 @@ function getAllData() {
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        
         displayData(data);
     })
     .catch(error => console.error('Error:', error));
 }
 
-function displayData(data) {
+function displayData(response) {
     var displayDiv = document.getElementById("displayData");
     displayDiv.innerHTML = "<h2>Received Data:</h2>";
 
-   
-    for (var key in data) {
-        if (data.hasOwnProperty(key)) {
-            var pElement = document.createElement("p");
-            pElement.innerHTML = key + ": " + data[key];
-            displayDiv.appendChild(pElement);
+    if (response && response.data) {
+  
+        if (Array.isArray(response.data)) {
+            response.data.forEach((dataObject, index) => {
+                var pElement = document.createElement("p");
+                pElement.innerHTML = "<strong>Student " + (index + 1) + ":</strong>";
+
+          
+                for (var key in dataObject) {
+                    if (dataObject.hasOwnProperty(key)) {
+                        var subElement = document.createElement("p");
+                        subElement.innerHTML = key + ": " + dataObject[key];
+                        pElement.appendChild(subElement);
+                    }
+                }
+
+                displayDiv.appendChild(pElement);
+            });
+        } else {
+        
+            for (var key in response.data) {
+                if (response.data.hasOwnProperty(key)) {
+                    var pElement = document.createElement("p");
+                    pElement.innerHTML = key + ": " + response.data[key];
+                    displayDiv.appendChild(pElement);
+                }
+            }
         }
+    } else {
+        
+        var errorElement = document.createElement("p");
+        errorElement.innerHTML = "<strong>Error:</strong> No data received from the server.";
+        displayDiv.appendChild(errorElement);
     }
 }
